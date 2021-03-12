90df0a2acf0a651b1376647ca1e2c42d85df57ba
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2021-03-12 17:40:05 -0600

    Update transaction.cpp

--- transaction.cpp
+++ transaction.cpp
@@ -14,3 +14,34 @@ transaction::transaction (string setFrom, string setTo, int setAmount)
 	amount=setAmount;
 
 }
+
+string transaction::getFrom()
+{
+	return from;
+}
+
+string transaction::getTo()
+{
+	return to;
+}
+
+int transaction::getAmount()
+{
+	return amount;
+}
+
+
+void transaction::setFrom(string s)
+{
+	from=s;
+}
+
+void transaction::setTo(string t)
+{
+	to=t;
+}
+
+void transaction::setAmount(int a)
+{
+	amount=a;
+}
