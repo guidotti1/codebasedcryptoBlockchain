750cb1e823a6e5f3dd010de67efd7728b5aae54c
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2021-03-12 17:39:48 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include"block.h"
 #include"blockchain.h"
 #include"sha256.h"
+#include"transaction.h"
+#include"encryption.h"
+#include<stdlib.h>
 
 using namespace std;
 
@@ -12,53 +15,94 @@ int main()
 
 
 //the user is able to choose their own block difficulty and the number of blocks that the chain will have 
-int blockDifficulty;
-int desiredReward;
-int desiredNumberBlocks;
+int blockDifficulty=0;
+int desiredReward=0;
+//int desiredNumberBlocks;
 cout<<"What would you like your block difficulty to be? "<<endl;
 cin>>blockDifficulty;
 cout<<"What would you like your block reward to be? "<<endl;
 cin>>desiredReward;
+
+/*
 cout<<"How many blocks do you want the chain to have? "<<endl;
 cin>>desiredNumberBlocks;
+*/
+
 blockchain mychain(blockDifficulty, desiredReward);
 
+
+transaction firstTransaction("address1", "address2", 100);
+transaction secondTransaction("address2", "address1", 50);
+mychain.addTransaction(firstTransaction);
+mychain.addTransaction(secondTransaction);
+
+cout<<"starting mining"<<endl;
+mychain.minePendingTransactions("myaddress");
+
+cout<<"My balance"<<endl;
+cout<<mychain.getBalanceOfAddress("myaddress")<<endl;
+
+
+mychain.minePendingTransactions("myaddress");
+cout<<"Checking again"<<endl;
+cout<<mychain.getBalanceOfAddress("myaddress")<<endl;
+
+
+encryption trial;
+trial.createMagmaFile();
+
+trial.readInput();
+
+
+
+
+
+
+/*
 cout<<"We are going to create a simple blockchain with " << desiredNumberBlocks << " blocks"<<endl;
-cout<<"All that we ask is that you enter in some mock data for each transaction "<<endl;
-int blockID=0;
-vector<string> passData;
-string nextTransaction;
+//cout<<"All that we ask is that you enter in some mock data for each transaction "<<endl;
+vector<int> amounts;
+int nextamt;
+
+
+while (mychain.getSize() != desiredNumberBlocks)
+{
+	amounts.clear();
+	cout<<"How many transactions you want to enter? (enter a sequence of amounts ending in -1)"<<endl;
+	cin>>nextamt;
+	while (nextamt != -1)
+	{
+		amounts.push_back(nextamt);
+		cin>>nextamt;
+	}
+	block rewardBlock = mychain.getLastBlock();
+	string rewardAddress = rewardBlock.getPrevHash();
+	transaction nextTransction(rewardAddress, nextamt)
+	mychain.minePendingTransactions();
+
+}
+string nextAmount;
+
 
 for (int i = 1; i < desiredNumberBlocks; i++)
 {
 	blockID++;
 	//get the information for this next transaction from the user
-	cout<<"Enter transaction information (amount) and press enter "<<endl;
-	cin>>nextTransaction;
+	cout<<"Enter transaction amount and press enter "<<endl;
+	cin>>nextAmount;
+	transaction newTransaction(nextAmount, i-1, i);
 	block recentBlock = mychain.getLastBlock();
 	//create new block using unique ID and transaction you just entered
-	mychain.addBlock(blockID, nextTransaction);
-	
-	block nextBlock=mychain.getLastBlock();
-	//cout<<"hash info for the block you just entered " << nextBlock.getCurrHash()<<endl;
+	mychain.addBlock(nextAmount);
 
-	//cout<<"hash info for previousBlock "<< recentBlock.getCurrHash()<<endl;
-	//cout<<"timestamp for this block you just entered "<< nextBlock.getTimestamp()<<endl;
-	//cout<<"ID for this block you just entered "<< nextBlock.getid()<<endl;
-	//cout<<"transactions information for the block you just entered " <<nextBlock.getTransaction()<<endl;
-	//cout<<endl<<endl;
+
+	block nextBlock=mychain.getLastBlock();
 }
 
-/*
-//mock version of messing with the chain, will give that the chain is invalid
-//we can change any parameter (id, timestamp, transactions, hash) to get invalid chain
-vector<block> change=mychain.getChain();
-change[1].setId(100);
-mychain.changeBlock(1, change[1]);
-*/
 
 
-/*
+
+
 if (mychain.isValid())
 {
 	cout<<"great job your blockchain is valid "<<endl;
