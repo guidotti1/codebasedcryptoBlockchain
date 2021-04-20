38373613bd7e59f5737b2a62b80a98017fdf561e
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2021-04-19 20:29:48 -0500

    Update hashingtest.cpp

--- newCBSignatures/hashingtest.cpp
+++ newCBSignatures/hashingtest.cpp
@@ -1,53 +1,17 @@
-#include<iostream>
-#include <fstream>
-#include "sha256.h"
-#include<bits/stdc++.h>
-#include<stdlib.h>
-#include<chrono>
-#include<unistd.h>
-using namespace std;
 
-//converts hash value to binary
-string convertHashToBinary(string hash);
-//converts a specific integer to ternary
-void convertToTernary(int N, string & res);
-//converts the whole hash to ternary
-void convertHashToTernary(string hash, string & res);
-//calculate weight (support size, number of nonzero positions) of a ternary string
-int calculateWeight(string ternaryString);
-
-void getcTomagma(string b, string q, string gfqRes);
+#include "hashingtest.h"
 
 
-int main()
-{
+/*
 int q = 7;
 int n = 400;
 int b = 218;
 int wsubC = 151;
 //hashing our message together with s_{y}
 //becomes c in the magma shit
-string toBeHashed  = "ourSampleMessage";
-toBeHashed += "(4 5 5 3 3 2 3 4 6 2)";
-string randomHash = sha256(toBeHashed);
-string res = "";
-int i = 0;
+
 auto begin = std::chrono::high_resolution_clock::now();
-do {
-    do
-        {
-            res = "";
-            //cout << "we are hasing " << toBeHashed << endl;
-            convertHashToTernary(randomHash, res);
-            //cout <<  res.size()<<endl;
-            toBeHashed += to_string(i);
-            randomHash = sha256(toBeHashed);
-            i++;
-        }
-    while (res.size() != b);
-    //cout << "res.size(): "<< res.size() << " calculateWeight(res): " << calculateWeight(res)<<endl;
-}
-while (calculateWeight(res) != wsubC);
+
 
 cout << "done"<<endl;
 cout << "we get the resulting hash fulfilling those conditions :  " << res << endl;
@@ -57,39 +21,88 @@ auto end = std::chrono::high_resolution_clock::now();
 auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
 printf("Time measured to do all those calculation: %.3f seconds.\n", elapsed.count() * 1e-9);
 
-string gfqRes = "";
-for (int i = 0; i < res.size(); i++)
-{
-    if (res[i] == '2')
-    {
-        gfqRes += "-1";
-    }
-    else if (res[i] == '1')
-    {
-        gfqRes += "1";
-    }
-    else if (res[i] == '0')
-    {
-        gfqRes += "0";
-    }
-}
-cout << "gfqRes(y): " << gfqRes<<endl;
-
-
-cout<<"putting that in the proper magma format"<<endl;
+cout<<"putting that shit in the proper magma format"<<endl;
 //putting c(resulting hash) in magma format
 getcTomagma(to_string(b), to_string(q), gfqRes);
+*/
 
-return 0;
+
+string hashingtest::getHashGF3()
+{
+	string toBeHashed  = message+ssuby;
+	string randomHash = sha256(toBeHashed);
+	string res = "";
+	int i = 0;
+	do {
+   		do
+        	{
+            	res = "";
+            	//cout << "we are hasing " << toBeHashed << endl;
+            	convertHashToTernary(randomHash, res);
+            	//cout <<  res.size()<<endl;
+            	toBeHashed += to_string(i);
+            	randomHash = sha256(toBeHashed);
+            	i++;
+        	}
+    	while (res.size() != b);
+    	//cout << "res.size(): "<< res.size() << " calculateWeight(res): " << calculateWeight(res)<<endl;
+	}
+	while (calculateWeight(res) != wsubC);
+	return res;
+
 }
 
-void getcTomagma(string b, string q, string gfqRes)
+string hashingtest::getHashGFq(string res)
+{
+	string gfqRes = "";
+	int negativeoneGFq = q-1;
+	
+	for (int i = 0; i < res.size(); i++)
+	{
+    	if (res[i] == '2')
+    	{
+        	gfqRes += to_string(negativeoneGFq);
+    	}
+   		else if (res[i] == '1')
+    	{
+        	gfqRes += '1';
+    	}
+   		else if (res[i] == '0')
+    	{
+     	   gfqRes += '0';
+    	}
+	}
+	return gfqRes;
+}
+
+
+hashingtest::hashingtest()
+{
+	q=0;
+	n=0;
+	b=0;
+	wsubC=0;
+	message="";
+}
+
+hashingtest::hashingtest(int setq, int setn, int setb, int setwsubC, string setmessage, string setssuby)
+{
+	q=setq;
+	n=setn;
+	b=setb;
+	wsubC=setwsubC;	
+	message=setmessage;
+	ssuby=setssuby;
+}
+
+
+void hashingtest::getcTomagma(string gfqRes)
 {
     ofstream o;
     o.open("gfqres.txt");
-    o << "q := "+q+";"<<endl;
+    o << "q := "+to_string(q)+";"<<endl;
     o << "F := GF(q);"<<endl;
-    o << "b := "+b+";"<<endl;
+    o << "b := "+to_string(b)+";"<<endl;
     o << "y := Random(VectorSpace(F, b));"<<endl;
     cout << "gfqres.size() : " << gfqRes.size() << endl;
     int counter = 0;
@@ -119,7 +132,7 @@ void getcTomagma(string b, string q, string gfqRes)
 
 }
 
-void convertHashToTernary(string hash, string & res)
+void hashingtest::convertHashToTernary(string hash, string & res)
 {
     //cout << "Ternary number of our hash value "
      //    << hash << " is: ";
@@ -155,7 +168,7 @@ void convertHashToTernary(string hash, string & res)
 }
 
 
-void convertToTernary(int N, string & res)
+void hashingtest::convertToTernary(int N, string & res)
 {
 
     if (N == 0)
@@ -184,7 +197,7 @@ void convertToTernary(int N, string & res)
 
 }
 
-string convertHashToBinary(string hash)
+string hashingtest::convertHashToBinary(string hash)
 {
 	string ans="";
 	for (size_t i = 0; i < hash.size(); ++i)
@@ -195,7 +208,7 @@ string convertHashToBinary(string hash)
 	return ans;
 }
 
-int calculateWeight(string ternaryString)
+int hashingtest::calculateWeight(string ternaryString)
 {
     int res = 0;
     for (int i = 0; i < ternaryString.size(); i++)
