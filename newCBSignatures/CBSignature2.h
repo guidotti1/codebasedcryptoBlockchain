d38276bc60d47455f5ca4b4c0de3ec99346478a5
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2021-05-02 12:57:31 -0400

    Create CBSignature2.h

---
+++ newCBSignatures/CBSignature2.h
@@ -0,0 +1,29 @@
+#ifndef CBSIGNATURE2_H_
+#define CBSIGNATURE2_H_
+
+#include<iostream>
+#include<vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+
+//stores new CB signature according to the algorithm - z and c
+class CBSignature2
+{
+
+public : 
+
+  vector<vector<int> > z;
+  vector<vector<int> > c;
+
+private :
+
+  //default constructor
+  CBSignature2();
+  //two argument constructor 
+  CBSignature2(vetor<vector<int> > setz, vector<vector<int> > setc);
+
+}
