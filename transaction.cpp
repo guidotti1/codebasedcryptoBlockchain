eea4ed1a927a8f123bfd26d6a328e49de685e8ec
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2021-05-02 21:04:13 -0400

    Update transaction.cpp

--- transaction.cpp
+++ transaction.cpp
@@ -12,28 +12,38 @@ transaction::transaction(newCBPublic setfrom, newCBPublic setto, int setAmount)
 {
 	fromkey=setfrom;
 	tokey=setto;
-	vector<vector<int> > usedH = fromkey.getHMatrix();
-	vector<vector<int> > usedS = fromkey.getSMatrix();
-	vector<vector<int> > usedE = tokey.getEMatrix();
-	for (int i = 0; i < usedH.size(); i++)
+	vector<vector<int> > fromKeyH = fromkey.getHMatrix();
+	vector<vector<int> > fromKeyS = fromkey.getSMatrix();
+	vector<vector<int> > toKeyH = tokey.getHMatrix();
+	vector<vector<int> > toKeyS = tokey.getSMatrix();
+	//storing all the fromkey information in a string so we can sha256() hash it
+	for (int i = 0; i < fromKeyH.size(); i++)
 	{
-		for (int j = 0; j < usedH[i].size(); j++)
+		for (int j = 0; j < fromKeyH[i].size(); j++)
 		{
-			from += to_string(usedH[i][j]);
+			from += to_string(fromKeyH[i][j]);
 		}
 	}
-	for (int i = 0; i < usedS.size(); i++)
+	for (int i = 0; i < fromKeyS.size(); i++)
 	{
-		for (int j = 0; j < usedS[i].size(); j++)
+		for (int j = 0; j < fromKeyS[i].size(); j++)
 		{
-			from += to_string(usedS[i][j]);
+			from += to_string(fromKeyS[i][j]);
 		}
 	}
-	for (int i = 0; i < usedE.size(); i++)
+	//same for the tokey information
+	for (int i = 0; i < toKeyH.size(); i++)
 	{
-		for (int j = 0; j < usedE[i].size(); j++)
+		for (int j = 0; j < toKeyH[i].size(); j++)
 		{
-			from += to_string(usedE[i][j]);
+			to += to_string(toKeyH[i][j]);
+		}
+	}
+	for (int i = 0; i < toKeyS.size(); i++)
+	{
+		for (int j = 0; j < toKeyS[i].size(); j++)
+		{
+			to += to_string(toKeyS[i][j]);
 		}
 	}
 	amount=setAmount;
@@ -41,7 +51,7 @@ transaction::transaction(newCBPublic setfrom, newCBPublic setto, int setAmount)
 	calculateHash();
 }
 	
-transaction::transaction (string setFrom, string setTo, int setAmount)
+transaction::transaction(string setFrom, string setTo, int setAmount)
 {
 	from=setFrom;
 	to=setTo;
@@ -50,6 +60,31 @@ transaction::transaction (string setFrom, string setTo, int setAmount)
 	calculateHash();
 }
 
+transaction::transaction(string setfrom, newCBPublic setto, int setAmount)
+{
+	from=setfrom;
+	tokey = setto;
+	amount=setAmount;
+	vector<vector<int> > toKeyH = tokey.getHMatrix();
+	vector<vector<int> > toKeyS = tokey.getSMatrix();
+	//storing all the tokey information in a string so we can sha256() hash it
+	for (int i = 0; i < toKeyH.size(); i++)
+	{
+		for (int j = 0; j < toKeyH[i].size(); j++)
+		{
+			from += to_string(toKeyH[i][j]);
+		}
+	}
+	for (int i = 0; i < toKeyS.size(); i++)
+	{
+		for (int j = 0; j < toKeyS[i].size(); j++)
+		{
+			from += to_string(toKeyS[i][j]);
+		}
+	}
+	hash="";
+	calculateHash();
+}
 /*
 void::transaction setKKSSig()
 {
@@ -122,8 +157,7 @@ void transaction::calculateHash()
 
 void transaction::signTransaction1()
 {
-	setKKSSig();
-	calculateHash();
+	//setKKSSig();
 }
 
 void transaction::signTransaction2(newCBPublic usedPublic, newCBPrivate usedPrivate)
@@ -132,22 +166,22 @@ void transaction::signTransaction2(newCBPublic usedPublic, newCBPrivate usedPriv
 	vector<vector<int> > useds = usedPublic.getSMatrix();
 	
 	string compare="";
-	string temp="";
+	string tobehashed="";
 	for (int i = 0; i < usedh.size(); i++)
 	{
 		for (int j = 0; j < usedh[i].size(); j++)
 		{
-			temp+=to_string(usedh[i][j]);
+			tobehashed+=to_string(usedh[i][j]);
 		}
 	}
 	for (int i = 0; i < useds.size(); i++)
 	{
 		for (int j = 0; j < useds[i].size(); j++)
 		{
-			temp+=to_string(useds[i][j]);
+			tobehashed+=to_string(useds[i][j]);
 		}
 	}
-	compare = sha256(temp);
+	compare = sha256(tobehashed);
 	
 	if (compare != from)
 	{
@@ -176,13 +210,24 @@ bool transaction::isTransactionValid2()
 	
 	newCBSignature temp(16381, 400, 100, 218, 156, 141, 151, 3420, 3375);
 	temp.setPublicKey(fromkey);
+
 	vector<vector<int> > usedz = usedsig.getz();
+	cout<<"z in valid program "<<endl;
+	for (int i =0; i < usedz.size(); i++)
+	{
+		for (int j =0; j <usedz[i].size(); i++)
+		{
+		cout<<usedz[i][j]<<endl;
+		}
+	}
+
 	vector<vector<int> > usedc = usedsig.getc();
 	temp.setZ(usedz);
 	temp.setC(usedc);
 	temp.setOurSig();
 	temp.setMessage(hash);
 	string res = temp.verifySignature();
+	cout<<"9"<<endl;
 	if (res=="passes")
 	{
 		cout<<"signature on the transaction is valid"<<endl;
