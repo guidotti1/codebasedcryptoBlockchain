288c836ea8ba4f4effbf730092bb42507090391e
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2021-05-02 21:03:13 -0400

    Update newCBPublic.cpp

--- newCBSignatures/newCBPublic.cpp
+++ newCBSignatures/newCBPublic.cpp
@@ -27,7 +27,13 @@ void newCBPublic::setHMatrix(vector<vector<int> > setto)
 {
 	HMatrix=setto;
 }
+
 void newCBPublic::setSMatrix(vector<vector<int> > setto)
 {
 	SMatrix=setto;
+}
+
+bool operator==(newCBPublic compare1, newCBPublic compare2)
+{
+	return ((compare1.HMatrix == compare2.HMatrix) && (compare1.SMatrix == compare2.SMatrix));
 }
